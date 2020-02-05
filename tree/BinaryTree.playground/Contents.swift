627c775404a77ea2519a287c34128e4352522c07
Zach Frew <zachary.frew@ascension-external.org>
2020-02-05 08:49:28 -0600

    Binary Tree setup

---
+++ tree/BinaryTree.playground/Contents.swift
@@ -0,0 +1,18 @@
+var tree: BinaryNode<Int> = {
+  let zero = BinaryNode(0)
+  let one = BinaryNode(1)
+  let five = BinaryNode(5)
+  let seven = BinaryNode(7)
+  let eight = BinaryNode(8)
+  let nine = BinaryNode(9)
+  
+  seven.leftChild = one
+  one.leftChild = zero
+  one.rightChild = five
+  seven.rightChild = nine
+  nine.leftChild = eight
+  
+  return seven
+}()
+
+print(tree)
---
+++ tree/BinaryTree.playground/Sources/BinaryNode.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+public class BinaryNode<Element> {
+    public var value: Element
+    public var leftChild: BinaryNode?
+    public var rightChild: BinaryNode?
+    
+    public init(_ value: Element) {
+        self.value = value
+    }
+}
+
+extension BinaryNode: CustomStringConvertible {
+    public var description: String { diagram(for: self) }
+    
+    private func diagram(for node: BinaryNode?,
+                         _ top: String = "",
+                         _ root: String = "",
+                         _ bottom: String = "") -> String {
+        guard let node = node else { return root + "nil\n" }
+        
+        if node.leftChild == nil && node.rightChild == nil {
+            return root + "\(node.value)\n"
+        }
+        
+        return diagram(for: node.rightChild,
+                           top + " ", top + "┌──", top + "│ ")
+                 + root + "\(node.value)\n"
+                 + diagram(for: node.leftChild,
+                           bottom + "│ ", bottom + "└──", bottom + " ")
+    }
+}
